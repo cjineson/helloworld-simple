a375a45d657d5ad78d938985324947cfb19e3d5e
Chris Ineson <chrisineson@gmail.com>
2020-08-03 19:36:22 +0100

    listen on $PORT or 8080

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+console.log(`Helloworld server starting, port ${process.env.PORT || 8080}`)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
