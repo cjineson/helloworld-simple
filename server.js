1192323cafe64e27f4be8556ea93f80c24ef9434
Chris Ineson <chris.ineson@lbgcts.onmicrosoft.com>
2021-03-11 23:34:28 +0000

    test

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 console.log(`Helloworld server starting, port ${process.env.PORT || 8080}`)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Hello World! :)');
   res.end();
 }).listen(process.env.PORT || 8080);
